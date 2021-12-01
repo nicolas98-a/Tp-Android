65166585b1f1bfae6ed21ecd1ab9d750ff2e62c8
Nicolas <acu.nicolas.1998@gmail.com>
2021-12-01 14:20:04 -0300

    pantalla detalle del complejo

--- tpIntegrador/app/src/main/java/com/unaj/loginsqlite/DetailComplex.kt
+++ tpIntegrador/app/src/main/java/com/unaj/loginsqlite/DetailComplex.kt
@@ -4,13 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import com.unaj.loginsqlite.R.drawable.iconsnoestacionar60
 import com.unaj.loginsqlite.model.Complex
 
 class DetailComplex : AppCompatActivity() {
 
     private lateinit var detailComplexName: TextView
     private lateinit var detailComplexPhone: TextView
+    private lateinit var detailComplexIvParking: ImageView
+    private lateinit var detailComplexIvLockerRoom: ImageView
+    private lateinit var detailComplexIvGrill: ImageView
+
     private lateinit var btnViewMap: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,12 +25,29 @@ class DetailComplex : AppCompatActivity() {
 
         detailComplexName = findViewById(R.id.actDetailComplexName)
         detailComplexPhone = findViewById(R.id.actDetailComplexPhone)
+        detailComplexIvParking = findViewById(R.id.ivComplexParking)
+        detailComplexIvLockerRoom = findViewById(R.id.ivComplexLockerRoom)
+        detailComplexIvGrill = findViewById(R.id.ivComplexGrill)
+
+
+
         btnViewMap = findViewById(R.id.viewMap)
 
         val complex = intent.getSerializableExtra("objectComplex") as Complex
 
         detailComplexName.text = complex.name
         detailComplexPhone.text = complex.phone
+
+        if (complex.parking == 0){
+            detailComplexIvParking.setImageResource(R.drawable.iconsnoestacionar60)
+        }
+        if (complex.lockerRoom == 0){
+            detailComplexIvLockerRoom.setImageResource(R.drawable.icons_no_changing_room_60)
+        }
+        if (complex.grill == 0){
+            detailComplexIvGrill.setImageResource(R.drawable.icons_no_parrilla_50)
+        }
+
 
 
         btnViewMap.setOnClickListener {
